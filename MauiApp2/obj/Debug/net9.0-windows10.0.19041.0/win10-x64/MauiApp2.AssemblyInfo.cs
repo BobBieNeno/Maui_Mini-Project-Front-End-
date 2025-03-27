04ca2f0381740b0d4623b0341044f1627bee9bbc
BobBieNeno <bie007x@gmail.com>
2025-03-27 18:21:57 +0700

    update2

--- MauiApp2/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MauiApp2.AssemblyInfo.cs
+++ MauiApp2/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MauiApp2.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+735aaaca564eb6a929924d7ce765233f26be898a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+366564b00a5618ad24fa9b0528e2487765e2d4be")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
